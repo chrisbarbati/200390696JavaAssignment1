b5202bc4c364d2a0dc95de7b8867e4c2ef75bd48
Christian <200390696@student.georgianc.on.ca>
2023-02-19 20:59:39 -0500

    Add instance variables, ArrayLists, default constructor...

--- src/main/java/com/example/javaassigment1_200390696/Student.java
+++ src/main/java/com/example/javaassigment1_200390696/Student.java
@@ -1,2 +1,91 @@
-package com.example.javaassigment1_200390696;public class Student {
+package com.example.javaassigment1_200390696;
+
+import javafx.scene.image.Image;
+
+import java.util.ArrayList;
+
+public class Student {
+    /**
+     * Two ArrayLists. validActivities holds the different activities that a student object is permitted to hold.
+     * validActivities is static so it's identical for each student
+     *
+     * studentActivities holds the different activities that each individual student enjoys.
+     */
+    private static ArrayList<String> validActivities = new ArrayList<>();
+    private ArrayList<String> studentActivities = new ArrayList<>();
+
+    /**
+     * Image object to hold the picture of the student
+     */
+    private Image studentPicture;
+
+    /**
+     * Variables to hold common student characteristics
+     */
+    private int studentNumber;
+    private String firstName;
+    private String lastName;
+
+    /**
+     * Default constructor
+     */
+    public Student(ArrayList<String> studentActivities, Image studentPicture, int studentNumber, String firstName, String lastName) {
+        setStudentActivities(studentActivities);
+        setStudentPicture(studentPicture);
+        setStudentNumber(studentNumber);
+        setFirstName(firstName);
+        setLastName(lastName);
+    }
+
+    /**
+     * Getters and Setters
+     */
+
+    public static ArrayList<String> getValidActivities() {
+        return validActivities;
+    }
+
+    public static void setValidActivities(ArrayList<String> validActivities) {
+        Student.validActivities = validActivities;
+    }
+
+    public ArrayList<String> getStudentActivities() {
+        return studentActivities;
+    }
+
+    public void setStudentActivities(ArrayList<String> studentActivities) {
+        this.studentActivities = studentActivities;
+    }
+
+    public Image getStudentPicture() {
+        return studentPicture;
+    }
+
+    public void setStudentPicture(Image studentPicture) {
+        this.studentPicture = studentPicture;
+    }
+
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+
+    public void setStudentNumber(int studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
--- src/main/java/com/example/javaassigment1_200390696/StudentCardController.java
+++ src/main/java/com/example/javaassigment1_200390696/StudentCardController.java
@@ -1,14 +1,23 @@
 package com.example.javaassigment1_200390696;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StudentCardController implements Initializable {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
