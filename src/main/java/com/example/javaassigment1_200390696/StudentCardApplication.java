0cafbc3497d33fd4098e1bfd2785a8f815701c20
Christian <200390696@student.georgianc.on.ca>
2023-02-26 18:57:41 -0500

    Add logos, improve styling

--- src/main/java/com/example/javaassigment1_200390696/StudentCardApplication.java
+++ src/main/java/com/example/javaassigment1_200390696/StudentCardApplication.java
@@ -3,6 +3,7 @@ package com.example.javaassigment1_200390696;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -12,6 +13,11 @@ public class StudentCardApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StudentCardApplication.class.getResource("studentCardView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
+        /**
+         * Set the logo in the taskbar and title bar
+         * Free to use image from https://pixabay.com/vectors/cap-school-graduation-1266204/
+         */
+        stage.getIcons().add(new Image("graduationCap.png"));
         stage.setTitle("Student Card");
         stage.setScene(scene);
         stage.show();
